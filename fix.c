a2090aa32357cf487f0437775ddf93dc70ce8eef
WangYinuo <yi_nuo_wang@hotmail.com>
2019-07-19 14:31:08 +0800

    修复bug
    Signed-off-by: WangYinuo <yi_nuo_wang@hotmail.com>

--- fix.c
+++ fix.c
@@ -5,23 +5,20 @@ char* str = "..dynsym..dynstr..hash..rel.dyn..rel.plt..plt..text@.ARM.extab..ARM
 char* str1 = "\0.dynsym\0.dynstr\0.hash\0.rel.dyn\0.rel.plt\0.plt\0.text@.ARM.extab\0.ARM.exidx\0.fini_array\0.init_array\0.dynamic\0.got\0.data\0.bss\0.shstrtab\0";
 Elf32_Shdr shdr[SHDRS] = { 0 };
 
-void get_elf_header(char* buffer,Elf32_Ehdr** pehdr)
-{
+void get_elf_header(char* buffer,Elf32_Ehdr** pehdr) {
 	int header_len = sizeof(Elf32_Ehdr);
 	memset(*pehdr, 0, header_len);
 	memcpy(*pehdr, (void*)buffer, header_len);
 }
 
-void get_program_table(Elf32_Ehdr ehdr,char* buffer,Elf32_Phdr** pphdr)
-{
+void get_program_table(Elf32_Ehdr ehdr,char* buffer,Elf32_Phdr** pphdr) {
 	int ph_size = ehdr.e_phentsize;
 	int ph_num = ehdr.e_phnum;
 	memset(*pphdr, 0, ph_size * ph_num);
-	memcpy(*pphdr, buffer + ehdr.e_phoff,ph_size * ph_num);
+	memcpy(*pphdr, buffer + ehdr.e_phoff, ph_size * ph_num);
 }
 
-long get_file_len(FILE* p)
-{
+long get_file_len(FILE* p) {
     fseek (p, 0, SEEK_END);
     long fsize = ftell (p);
     rewind (p);
@@ -29,49 +26,56 @@ long get_file_len(FILE* p)
 }
 
 
-void get_Info(Elf32_Phdr* phdr, Elf32_Ehdr *pehdr, char* buffer)
-{
+void get_Info(Elf32_Phdr* phdr, Elf32_Ehdr* pehdr, char* buffer) {
 	Elf32_Dyn* dyn = NULL;
 	Elf32_Dyn* d = NULL;
-	Elf32_Phdr load = { 0 };
+	Elf32_Phdr load = {0};
 	
 	int ph_num = pehdr->e_phnum;
-	int dyn_size = 0, dyn_off = 0;
-	int nbucket = 0, nchain = 0;
-	int flag = 0, i = 0;
+	int dyn_size = 0;
+	int dyn_off = 0;
+	int nbucket = 0;
+	int nchain = 0;
+	int flag = 0;
 
-	for(;i < ph_num;i++) {
+	int i = 0;
+	for(; i < ph_num; i++) {
 		if (phdr[i].p_type == PT_LOAD) {
 			if (phdr[i].p_vaddr > 0x0) {
+				printf("get PT_LOAD\n");
 				load = phdr[i];
-				shdr[BSS].sh_name = strstr(str,".bss") - str;
+				shdr[BSS].sh_name = strstr(str, ".bss") - str;
 				shdr[BSS].sh_type = SHT_NOBITS;
 				shdr[BSS].sh_flags = SHF_WRITE | SHF_ALLOC;
 				shdr[BSS].sh_addr =  phdr[i].p_vaddr + phdr[i].p_filesz;
 				shdr[BSS].sh_offset = shdr[BSS].sh_addr - 0x1000;
 				shdr[BSS].sh_addralign = 1;
-
 				continue;
 			}
 		}
 
 		if(phdr[i].p_type == PT_DYNAMIC) {
+			printf("get PT_DYNAMIC\n");
 			shdr[DYNAMIC].sh_name = strstr(str, ".dynamic") - str;
 			shdr[DYNAMIC].sh_type = SHT_DYNAMIC;
 			shdr[DYNAMIC].sh_flags = SHF_WRITE | SHF_ALLOC;
 			shdr[DYNAMIC].sh_addr = phdr[i].p_vaddr;
-			shdr[DYNAMIC].sh_offset = phdr[i].p_offset;
+			shdr[DYNAMIC].sh_offset = phdr[i].p_offset + 0x1000;
 			shdr[DYNAMIC].sh_size = phdr[i].p_filesz;
 			shdr[DYNAMIC].sh_link = 2;
 			shdr[DYNAMIC].sh_info = 0;
 			shdr[DYNAMIC].sh_addralign = 4;
 			shdr[DYNAMIC].sh_entsize = 8;
 			dyn_size = phdr[i].p_filesz;
-    		dyn_off = phdr[i].p_offset;
+			// 之前使用  phdr[i].p_offset 字段获取 dyn_off ，是有问题的
+			// 从内存中dump下来的数据 应使用vaddr字段
+    		dyn_off = phdr[i].p_vaddr;
+			
     		continue;
 		}
 
 		if(phdr[i].p_type == PT_LOPROC || phdr[i].p_type == PT_LOPROC + 1) {
+			printf("get PT_LOPROC\n");
 			shdr[ARMEXIDX].sh_name = strstr(str, ".ARM.exidx") - str;
 			shdr[ARMEXIDX].sh_type = SHT_LOPROC;
 			shdr[ARMEXIDX].sh_flags = SHF_ALLOC;
@@ -85,13 +89,15 @@ void get_Info(Elf32_Phdr* phdr, Elf32_Ehdr *pehdr, char* buffer)
 			continue;
 		}
 	}
-
+	printf("dyn_off=%d, dyn_size=%d", dyn_off, dyn_size);
 	dyn = (Elf32_Dyn*)malloc(dyn_size);
-	memcpy(dyn,buffer+dyn_off,dyn_size);
+	memcpy(dyn, buffer + dyn_off, dyn_size);
 	i = 0;
 	for (; i < dyn_size / sizeof(Elf32_Dyn); i++) {
-		switch (dyn[i].d_tag) {
+		printf("tag=%d\n", dyn[i].d_tag);
+		switch(dyn[i].d_tag) {
 			case DT_SYMTAB:
+				printf("get DT_SYMTAB\n");
 				shdr[DYNSYM].sh_name = strstr(str, ".dynsym") - str;
 				shdr[DYNSYM].sh_type = SHT_DYNSYM;
 				shdr[DYNSYM].sh_flags = SHF_ALLOC;
@@ -104,6 +110,7 @@ void get_Info(Elf32_Phdr* phdr, Elf32_Ehdr *pehdr, char* buffer)
 				break;
 
 			case DT_STRTAB:
+				printf("get DT_STRTAB\n");
 				shdr[DYNSTR].sh_name = strstr(str, ".dynstr") - str;
 				shdr[DYNSTR].sh_type = SHT_STRTAB;
 				shdr[DYNSTR].sh_flags = SHF_ALLOC;
@@ -114,6 +121,7 @@ void get_Info(Elf32_Phdr* phdr, Elf32_Ehdr *pehdr, char* buffer)
 				break;
 
 			case DT_HASH:
+				printf("get DT_HASH\n");
 				shdr[HASH].sh_name = strstr(str, ".hash") - str;
 				shdr[HASH].sh_type = SHT_HASH;
 				shdr[HASH].sh_flags = SHF_ALLOC;
@@ -129,6 +137,7 @@ void get_Info(Elf32_Phdr* phdr, Elf32_Ehdr *pehdr, char* buffer)
 				break;
 
 			case DT_REL:
+				printf("get DT_REL\n");
 				shdr[RELDYN].sh_name = strstr(str, ".rel.dyn") - str;
 				shdr[RELDYN].sh_type = SHT_REL;
 				shdr[RELDYN].sh_flags = SHF_ALLOC;
@@ -141,6 +150,7 @@ void get_Info(Elf32_Phdr* phdr, Elf32_Ehdr *pehdr, char* buffer)
 				break;
 
 			case DT_JMPREL:
+				printf("get DT_JMPREL\n");
 				shdr[RELPLT].sh_name = strstr(str, ".rel.plt") - str;
 				shdr[RELPLT].sh_type = SHT_REL;
 				shdr[RELPLT].sh_flags = SHF_ALLOC;
@@ -153,10 +163,12 @@ void get_Info(Elf32_Phdr* phdr, Elf32_Ehdr *pehdr, char* buffer)
 				break;
 
 			case DT_PLTRELSZ:
+				printf("get DT_PLTRELSZ\n");
 				shdr[RELPLT].sh_size = dyn[i].d_un.d_val;
 				break;
 
 			case DT_FINI:
+				printf("get DT_FINI\n");
 				shdr[FINIARRAY].sh_name = strstr(str, ".fini_array") - str;
 				shdr[FINIARRAY].sh_type = 15;
 				shdr[FINIARRAY].sh_flags = SHF_WRITE | SHF_ALLOC;
@@ -167,6 +179,7 @@ void get_Info(Elf32_Phdr* phdr, Elf32_Ehdr *pehdr, char* buffer)
 				break;
 
 			case DT_INIT:
+				printf("get DT_INIT\n");
 				shdr[INITARRAY].sh_name = strstr(str, ".init_array") - str;
 				shdr[INITARRAY].sh_type = 14;
 				shdr[INITARRAY].sh_flags = SHF_WRITE | SHF_ALLOC;
@@ -177,6 +190,7 @@ void get_Info(Elf32_Phdr* phdr, Elf32_Ehdr *pehdr, char* buffer)
 				break;
 
 			case DT_RELSZ:
+				printf("get DT_RELSZ\n");
 				shdr[RELDYN].sh_size = dyn[i].d_un.d_val;
 				break;
 			
@@ -185,6 +199,7 @@ void get_Info(Elf32_Phdr* phdr, Elf32_Ehdr *pehdr, char* buffer)
 				break;
 
 			case DT_PLTGOT:
+				printf("get DT_PLTGOT\n");
 				shdr[GOT].sh_name = strstr(str, ".got") - str;
 				shdr[GOT].sh_type = SHT_PROGBITS;
 				shdr[GOT].sh_flags = SHF_WRITE | SHF_ALLOC; 
@@ -236,12 +251,12 @@ void get_Info(Elf32_Phdr* phdr, Elf32_Ehdr *pehdr, char* buffer)
 	shdr[STRTAB].sh_addralign = 1;
 }
 
-int main(int argc, char const *argv[])
-{
-	FILE* fr = NULL,* fw = NULL;
+int main(int argc, char const* argv[]) {
+	FILE* fr = NULL;
+	FILE* fw = NULL;
 	long flen = 0,result = 0;
 	char* buffer = NULL;
-	Elf32_Ehdr *pehdr = NULL;
+	Elf32_Ehdr* pehdr = NULL;
 	Elf32_Phdr* pphdr = NULL;
 
 	if (argc < 2) {
@@ -258,6 +273,7 @@ int main(int argc, char const *argv[])
 	flen = get_file_len(fr);
 
 	buffer = (char*)malloc(sizeof(char)*flen);
+	memset(buffer, 0, flen);
 	if (buffer == NULL) {
 		printf("Malloc error\n");
 		goto error;
@@ -292,7 +308,7 @@ int main(int argc, char const *argv[])
 	memcpy(buffer + shdr[STRTAB].sh_offset, str1, strlen(str) + 1);
 	memcpy(buffer + pehdr->e_shoff, shdr, pehdr->e_shentsize * pehdr->e_shnum);
 	flen = shdr[STRTAB].sh_offset + strlen(str) + 1 + SHDRS * sizeof(Elf32_Shdr);
-	fwrite(buffer, sizeof(char)*flen, 1, fw);
+	fwrite(buffer, sizeof(char) * flen, 1, fw);
 
 error:
 	if(fw != NULL)
